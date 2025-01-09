866340297d0634663e4e291ff1615a39bdc8e97f
= <=>
2025-01-09 23:06:23 +0530

    Add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { Analytics } from '@vercel/analytics/react';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Component {...pageProps} />
+            <Analytics />
         </>
     );
 } 
